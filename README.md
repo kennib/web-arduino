9c93f4272bdfe06241ac1d8390f0fb249e815141
Kenni Bawden <kenni.bawden@gmail.com>
2016-12-23 14:10:13 +1030

    Added a simple server for uploading code

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Write your Arduino code in a web browser and have it compiled and uploaded to a 
 ## Setup
 
  1. Install https://github.com/sudar/Arduino-Makefile
+ 2. `pip3 install bottle cherrypy pyserial`
+ 3. Edit config.ini
+ 4. `python3 server.py`
---
+++ server.py
@@ -0,0 +1,117 @@
+#/usr/bin/env python3
+
+import subprocess
+from os import listdir, chdir, path
+from bottle import route, post, request, run, template
+
+# Get web arduino config
+from configparser import SafeConfigParser
+
+config = SafeConfigParser()
+config.read('config.ini')
+
+ARDUINO_DIR = config.get('arduino', 'arduino_dir')
+DEVICE_DIR = config.get('arduino', 'device_dir')
+DEVICE_PREFIX = config.get('arduino', 'device_prefix')
+BOARD_TAG = config.get('arduino', 'board_tag')
+BOARD_SUB = config.get('arduino', 'board_sub')
+
+MAKEFILE = config.get('arduino', 'makefile_path')
+CODE_DIR = config.get('arduino', 'code_dir')
+
+HOST = config.get('web', 'host')
+PORT = config.get('web', 'port')
+
+BOARD_LIST = '''
+<ul>
+% for board in boards:
+	<li>
+		<a href="/{{board}}">Board {{ board }}</a>
+	</li>
+% end
+</ul>
+% if len(boards) == 0:
+	<h2>No boards connected</h2>
+% end
+'''
+
+CODE_INPUT = '''
+<form method="POST">
+	<textarea name=code style="width:100%; height:80%;">{{ code }}</textarea>
+	<input type="submit" value="Upload"></input>
+</form>
+'''
+
+BLINK_CODE = '''
+void setup() {
+	pinMode(13, OUTPUT);
+}
+
+void loop() {
+	digitalWrite(13, HIGH);
+	delay(1000);
+	digitalWrite(13, LOW);
+	delay(1000);
+}
+'''
+
+# Index page
+@route('/')
+def index():
+	devices = listdir(path=DEVICE_DIR)
+	boards = [device.lstrip(DEVICE_PREFIX) for device in devices if device.startswith(DEVICE_PREFIX)]
+	return template(BOARD_LIST, boards=boards)
+
+# Code writing page (per board)
+@route('/<serial:re:\w+>')
+def code(serial):
+    return template(CODE_INPUT, code=BLINK_CODE)
+
+# Code submission page (per board)
+@post('/<serial:re:\w+>')
+def upload(serial):
+	# The submitted code
+	code = request.params['code']
+
+	# Get absolute paths for the makefile, code and board
+	makefile = path.abspath(MAKEFILE)
+	code_dir = path.abspath(CODE_DIR)
+	board_dir = path.abspath(path.join(code_dir, serial))
+	device = path.join(DEVICE_DIR, DEVICE_PREFIX+serial)
+
+	# Create a directory for the code
+	if path.commonpath([code_dir, board_dir]) == code_dir:
+		subprocess.run(['mkdir', '-p', board_dir], check=True)
+		subprocess.run(['ln', '-F', '-s', makefile, board_dir])
+
+		# Write, compile and upload the code
+		with open(path.join(board_dir, 'Code.ino'), 'w') as code_file:
+			code_file.write(code)
+
+		build = subprocess.run(['make',
+			'-C', board_dir,
+			'ARDUINO_DIR='+ARDUINO_DIR,
+			'BOARD_TAG='+BOARD_TAG,
+			'BOARD_SUB='+BOARD_SUB,
+			'MONITOR_PORT='+device
+		],
+		check=True)
+		#stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		upload = subprocess.call(['make',
+			'-C', board_dir,
+			'upload',
+			'ARDUINO_DIR='+ARDUINO_DIR,
+			'BOARD_TAG='+BOARD_TAG,
+			'BOARD_SUB='+BOARD_SUB,
+			'MONITOR_PORT='+device
+		],
+		)
+		#check=True)
+		#stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+	return template(CODE_INPUT, code=code)
+
+
+if __name__ == '__main__':
+	run(server='cherrypy', host=HOST, port=PORT, reloader=True)
